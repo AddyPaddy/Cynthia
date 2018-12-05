fd44a726b6ae94631208bd1f4c072f3b5926951a
AddyPaddy <adarshrockzexe@gmail.com>
2018-12-05 20:41:45 +0530

    added $8pool feature with 5 possible outcomes

--- Program.cs
+++ Program.cs
@@ -17,14 +17,45 @@ namespace OsuIndiaBot
         {
             discord = new DiscordClient(new DiscordConfiguration
             {
-                Token = "<BOT Token>",
+                Token = "<Bot Token>",
                 TokenType = TokenType.Bot
             });
 
             discord.MessageCreated += async e =>
             {
-                if (e.Message.Content.ToLower().StartsWith("ping"))
+                if (e.Message.Content.ToLower().StartsWith("$ping"))
                     await e.Message.RespondAsync("pong!");
+            };
+
+            discord.MessageCreated += async e =>
+            
+            {
+                if (e.Message.Content.ToLower().StartsWith("$8pool"))
+                  
+                 {
+                     Random random = new Random();
+                     var rnd = random.Next(1, 5);
+                 if(rnd==1)
+                 {
+                     await e.Message.RespondAsync("Yes!");
+                 }
+                  if(rnd==2)
+                 {
+                     await e.Message.RespondAsync("Maybe?");
+                 }
+                  if(rnd==3)
+                 {
+                     await e.Message.RespondAsync("Hmmmmmmm");
+                 }
+                  if(rnd==4)
+                 {
+                     await e.Message.RespondAsync("I press X... I doubt");
+                 }
+                  if(rnd==5)
+                 {
+                     await e.Message.RespondAsync("No!");
+                 }
+                 }
             };
 
             await discord.ConnectAsync();
